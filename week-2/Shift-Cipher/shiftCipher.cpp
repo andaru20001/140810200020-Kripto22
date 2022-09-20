610a9526f4e30ca54784c293783816397b259319
Andaru <andarudanurdara@yahoo.com>
2022-09-20 12:28:58 +0700

    Update pengumpulan tugas

--- week-2/Shift-Cipher/shiftCipher.cpp
+++ week-2/Shift-Cipher/shiftCipher.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-    char ptext[100];
+    char text[100];
     int key, pil;
 
-    std::cout << "Input Plaintext : ";
-    std::cin.getline(ptext, 100);
+    std::cout << "Input Plaintext/Ciphertext : ";
+    std::cin.getline(text, 100);
 
     std::cout << "Input key : ";
     std::cin >> key;
@@ -16,33 +16,47 @@ int main() {
 
     if(pil == 1) {
         char a;
-        for(int i = 0; ptext[i] != '\0'; i++) {
-            a = ptext[i];
+        for(int i = 0; text[i] != '\0'; i++) {
+            a = text[i];
 
             if(a >= 'a' && a <= 'z') {
                 a = (a + key - 97)% 26 + 97;
-                ptext[i] = a;
+                text[i] = a;
             } else if (a >= 'A' && a <= 'Z') {
                 a = (a + key - 65)% 26 + 65;
-                ptext[i] = a;
+                text[i] = a;
             }
         }
-        std::cout << "\nEnkripsi : " << ptext;
+        std::cout << "\nEnkripsi : " << text;
     } 
     
     else if (pil == 2) {
         char a;
-        for(int i = 0; ptext[i] != '\0'; i++) {
-            a = ptext[i];
+        for(int i = 0; text[i] != '\0'; i++) {
+            a = text[i];
 
             if(a >= 'a' && a <= 'z') {
-                a = (a - key - 97)% 26 + 97;
-                ptext[i] = a;
-            } else if (a >= 'A' && a <= 'Z') {
-                a = (a - key - 65)% 26 + 65;
-                ptext[i] = a;
+                if (a - 97 - key < 0) {
+                    a = (a - 97 - key + 26) % 26 + 97;
+                    text[i] = a;
+                } 
+                else {
+                    a = (a - 97 - key) % 26 + 97;
+                    text[i] = a;
+                }
+            } 
+            
+            else if (a >= 'A' && a <= 'Z') {
+                if (a - 65 - key < 0){
+                    a = (a - 65 - key + 26) % 26 + 65;
+                    text[i] = a;
+                } 
+                else {
+                    a = (a - 65 - key) % 26 + 65;
+                    text[i] = a;
+                }
             }
         }
-        std::cout << "\nDekripsi : " << ptext;
+        std::cout << "\nDekripsi : " << text;
     }
 }
